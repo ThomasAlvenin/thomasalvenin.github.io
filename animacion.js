31ce2b93ecfac521ed706e48c696e673659c0a85
Thomas Alvenin <105067427+ThomasAlvenin@users.noreply.github.com>
2023-03-29 15:32:37 +0200

    Delete animacion.js

--- animacion.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(link => {
-  link.addEventListener('mouseover', () => {
-    link.classList.add('nav-active');
-  });
-  link.addEventListener('mouseout', () => {
-    link.classList.remove('nav-active');
-  });
-});
